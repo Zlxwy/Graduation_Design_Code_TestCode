643c969243497921d8ef2816e3c9eb7f62957d50
Zlxwy <y3021330020@163.com>
2026-02-25 04:17:28 +0800

    delete all

--- Test-Logic/Test-CRC16/r.bat
+++
@@ -1,4 +0,0 @@
-chcp 65001
-gcc CRC16.c -o r.exe
-r.exe
-pause
--- Test-RDKX5/C++/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(void) {
-  std::cout << "hello" << " " << "world" << std::endl;
-  return 0;
-}
